79714d2e2ebd09932eb6c563fca3eff7af1541e8
tanprograma <tanprograma@gmaol.com>
2023-12-31 23:58:21 +0300

    ev

--- index.mjs
+++ index.mjs
@@ -2,7 +2,9 @@ import express from "express";
 import path from "path";
 const app = express();
 app.use(express.static("public"));
-
+app.get("/", (res, res) => {
+  res.sendFile("/public/index.html");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("server actively listening");
